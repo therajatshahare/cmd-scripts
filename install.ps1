331dba819729706b54fa7fa723ade21a2d911af3
RAJAT SHAHARE <therajatshahare@gmail.com>
2026-04-13 15:21:30 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -136,7 +136,7 @@ function folders  { & "`$scriptDir\folders.ps1" @args }
 function insta    { & "`$scriptDir\insta.ps1" @args }
 
 function toolkit-version {
-    Write-Host "Cmd-Scripts Version: `$toolkitVersion"
+    Write-Host "Cmd-Scripts Version: $($toolkitVersion)"
 }
 
 function update-scripts {
